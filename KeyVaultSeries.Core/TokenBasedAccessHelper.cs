c8a927e94782e72ed6b927b2e1867b0a14ad1b43
mikkelhm <mikkel@umbraco.com>
2018-10-17 09:19:17 +0200

    Add part 3

---
+++ KeyVaultSeries.Core/TokenBasedAccessHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyVaultSeries.Core
+{
+    /// <summary>
+    /// From https://docs.microsoft.com/en-us/azure/key-vault/key-vault-use-from-web-application
+    /// </summary>
+    public static class TokenBasedAccessHelper
+    {
+        public static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(ConfigurationManager.AppSettings["ClientId"],
+                        ConfigurationManager.AppSettings["ClientSecret"]);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+    }
+}
---
+++ part 3 - get secret from Key Vault in your app/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using KeyVaultSeries.Core;
+using Microsoft.Azure.KeyVault;
+using System;
+
+namespace GetSecretsInYourApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            KeyVaultClient keyVaultClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    TokenBasedAccessHelper.GetToken));
+            keyVaultClient.GetSecretAsync("url-to-secret");
+        }
+    }
+}
