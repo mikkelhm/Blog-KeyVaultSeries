1e5b439b0d5ecc4b7d0524f02c552de4061f45a7
mikkelhm <mikkel@umbraco.com>
2018-10-18 22:02:26 +0200

    Fully example in part 3

--- part 3 - get secret from Key Vault in your app/Global.asax.cs
+++ part 3 - get secret from Key Vault in your app/Global.asax.cs
@@ -6,13 +6,15 @@ namespace GetSecretsInYourApp
 {
     public class Global : System.Web.HttpApplication
     {
-
         protected void Application_Start(object sender, EventArgs e)
         {
             KeyVaultClient keyVaultClient = new KeyVaultClient(
                 new KeyVaultClient.AuthenticationCallback(
                     TokenBasedAccessHelper.GetToken));
-            keyVaultClient.GetSecretAsync("url-to-secret");
+
+            var secretValut = keyVaultClient.GetSecretAsync("[url-to-secret]")
+                // Don't try this at home :)
+                .GetAwaiter().GetResult().Value;
         }
     }
 }
